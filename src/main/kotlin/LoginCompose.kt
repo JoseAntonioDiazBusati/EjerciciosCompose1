c803c3aecb9997afa6afb981603f685c3896fadf
JoseAntonio <jdiabus511@g.educaand.es>
2024-04-25 15:08:33 +0200

    Login

--- src/main/kotlin/LoginCompose.kt
+++ src/main/kotlin/LoginCompose.kt
@@ -1,6 +1,9 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.HoverInteraction
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -8,15 +11,18 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import kotlin.system.exitProcess
 
-@Composable
+
 @Preview
 fun main()= application {
     val windowState = rememberWindowState(size = DpSize(1200.dp,800.dp))
@@ -31,8 +37,8 @@ fun main()= application {
 
 @Composable
 fun PantallaInicio(){
-    var usuario = remember { mutableStateOf(TextFieldValue("")) }
-    var password = remember { mutableStateOf(TextFieldValue("")) }
+    val usuario = remember { mutableStateOf(TextFieldValue("")) }
+    val password = remember { mutableStateOf(TextFieldValue("")) }
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -42,25 +48,30 @@ fun PantallaInicio(){
     ){
         OutlinedTextField(
             value = usuario.value,
-            label = { Text(text = "Usuario")},
-            onValueChange = { usuario = it },
+            label = { Text(text = "Nombre del usuario")},
+            onValueChange = { usuario.value = it },
             modifier = Modifier
-                .width(100.dp)
-                .height(200.dp)
+                .width(200.dp)
+                .height(75.dp)
         )
         OutlinedTextField(
             value = password.value,
-            label = { Text(text = "Contraseña")},
-            onValueChange = { password = it },
+            label = { Text(text = "Introduzca contraseña")},
+            onValueChange = { password.value = it },
+            visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier
-                .width(100.dp)
-                .height(200.dp)
-            )
+                .width(200.dp)
+                .height(75.dp)
+        )
+        Button(
+            onClick ={},
+        ){
+            Text(text = "Login")
+        }
+        Button(
+            onClick ={ exitProcess(0)},
+        ){
+            Text(text = "Salir")
+        }
     }
-}
-
-
-@Composable
-fun InicioSesion(usuario:String, password:String){
-
 }
